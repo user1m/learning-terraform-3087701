7d6390a2fd21747563f237a1b998b7cad0c551d9
Claudius Mbemba <mbembac@gmail.com>
2023-08-12 21:16:53 -0700

    use module for sec group

--- main.tf
+++ main.tf
@@ -21,10 +21,30 @@ data "aws_vpc" "default" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [ aws_security_group.web.id ]
+  vpc_security_group_ids = [ 
+    # aws_security_group.web.id 
+    # https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=outputs
+    module.web_sg.security_group_id
+  ]
   tags = {
     Name = "HelloWorld"
   }
+}
+
+module "web_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name = "web_new_sec"
+  
+  vpc_id      = data.aws_vpc.default.id
+
+  # https://github.com/terraform-aws-modules/terraform-aws-security-group/blob/master/rules.tf
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  # https://registry.terraform.io/modules/terraform-aws-modules/security-group/aws/latest?tab=inputs
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "web" {
